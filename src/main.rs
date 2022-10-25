2736863014bd6c5ae6b61471580c3855d0ab3e6a
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-10-25 13:38:24 +0300

    Add: kind of a Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+
+use serenity::{
+    prelude::*,
+    model::prelude::*,
+    framework::StandardFramework, Client,
+    framework::standard::{macros::{command,group}, CommandResult}
+};
+
+use serenity::async_trait;
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.channel_id.say(&ctx.http, "Ping!").await?;
+
+    Ok(())
+}
+
+#[group]
+#[commands(ping)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    // fn message(&self, ctx: Context, msg: Message) {
+        // unimplemented!();
+    // }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), ()> {
+    let token = std::env::var("QUEENSCORSAR_TOKEN").expect("Couldn't fetch the API token from the environment");
+    let framework = StandardFramework::new()
+        .configure(|c| c.prefix("!"))
+        .group(&GENERAL_GROUP);
+    // let mut client = Client::new(token, Handler).expect("Couldn't create the new client!");
+    
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework);
+
+    Ok(())
 }
