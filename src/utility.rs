d9c5525651c666c417c6c2387f9b4451193d7f8a
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-01 15:35:48 +0300

    Add: documentation for utility functions

--- src/utility.rs
+++ src/utility.rs
@@ -10,6 +10,13 @@ use std::{
 
 use std::sync::Arc;
 
+/// Запроса ввода от пользователя
+///
+/// Данная функция сначала представляет пользователю
+/// указанное сообщение, затем ожидает его ответа в
+/// течении 120 секунд и возвращает результат ввода.
+/// Если же пользователь так и не ответил, возвращается
+/// ошибка типа `BotError::TimedOut`
 pub async fn query_from_user(ctx: &Context, user: &User, msg: &str) -> UResult<String> {
     let dm = user.create_dm_channel(&ctx.http).await?;
 
@@ -25,6 +32,7 @@ pub async fn query_from_user(ctx: &Context, user: &User, msg: &str) -> UResult<S
     reply.ok_or(BotError::TimedOut.into())
 }
 
+/// Проверка присутствия пользователя в указанной группе
 pub async fn user_is_in_guild(ctx: &Context, user: &User, gid: &GuildId) -> UResult<bool> {
     let r = ctx
         .http
@@ -33,6 +41,7 @@ pub async fn user_is_in_guild(ctx: &Context, user: &User, gid: &GuildId) -> URes
     Ok(r.len() > 0)
 }
 
+/// Отправка личного сообщения пользователю
 pub async fn send_privately(ctx: &Context, user: &User, msg: &str) -> UResult {
     let private = user.create_dm_channel(&ctx.http).await?;
     private.send_message(&ctx.http, |m| {
@@ -41,11 +50,13 @@ pub async fn send_privately(ctx: &Context, user: &User, msg: &str) -> UResult {
     Ok(())
 }
 
+/// Тип-ключ для специального типизированного контейнера `TypeMap`
 pub struct Session;
 impl TypeMapKey for Session {
     type Value = HashMap<UserId, bool>;
 }
 
+/// Сохранение пары значений ID пользователя и булевого флага
 pub async fn push_session(ctx: &Context, uid: UserId, value: bool) {
     let mut data = ctx.data.write().await;
     let sessions = data.entry::<Session>().or_insert(HashMap::new());
@@ -53,6 +64,7 @@ pub async fn push_session(ctx: &Context, uid: UserId, value: bool) {
     *entry = value;
 }
 
+/// Удаление сессионного булевого флага связанного с указанным ID пользователя
 pub async fn pop_session(ctx: &Context, uid: UserId) {
     let mut data = ctx.data.write().await;
     let sessions = data.entry::<Session>().or_insert(HashMap::new());
