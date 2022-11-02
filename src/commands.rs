618e257d04fe12363255efb9bff66b2e0b72cf61
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-02 14:11:26 +0300

    Add: log messages for the rules command

--- src/commands.rs
+++ src/commands.rs
@@ -8,7 +8,8 @@ use serenity::{
     utils::MessageBuilder,
 };
 
-use crate::{core::start_signup_session, prelude::send_privately};
+use crate::core::start_signup_session;
+use crate::prelude::*;
 
 /// Команда проверки связи с ботом
 #[command]
@@ -21,6 +22,13 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 /// Команда запроса правил сервера и запуска процесса регистрации
 #[command]
 async fn rules(ctx: &Context, msg: &Message) -> CommandResult {
+    let logger = child_logger(ctx).await?;
+    info!(logger, "Executing 'rules' command";
+        "initiator nabe" => &msg.author.name,
+        "guild id" => msg.guild_id.unwrap().0,
+        "initiator id" => msg.author.id.0
+    );
+
     let user = &msg.author;
 
     let response = MessageBuilder::new()
@@ -30,17 +38,16 @@ async fn rules(ctx: &Context, msg: &Message) -> CommandResult {
         .build();
     msg.channel_id.say(&ctx.http, response).await?;
 
-    match start_signup_session(&ctx, &user, &msg.guild_id.unwrap()).await {
-        Err(why) => {
-            let msg =
-                "Ого! Что-то дало сбой... Пожалуйста не забудь сообщить об этом случае Иннри!";
-            send_privately(ctx, user, msg).await?;
-            println!("[rules]: Something went wrong: {:?}", why);
-        }
-        _ => (),
-    };
-
-    Ok(())
+    debug!(logger, "Starting sign up session");
+    if let Err(why) = start_signup_session(&ctx, &user, &msg.guild_id.unwrap()).await {
+        let msg =
+            "Ого! Что-то дало сбой... Пожалуйста не забудь сообщить об этом случае Иннри!";
+        send_privately(ctx, user, msg).await?;
+        error!(logger, "Could not successfully register the user"; "reason" => format!("{:?}", why));
+        Err(why.into())
+    } else {
+        Ok(())
+    }
 }
 
 /// Структура с основными командами бота
