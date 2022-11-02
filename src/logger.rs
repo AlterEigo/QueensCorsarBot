20a52ef2a0227a66aca619c2885fa75c8800ea10
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-02 12:08:50 +0300

    Add: slog configuration functions

--- src/logger.rs
+++ src/logger.rs
@@ -1,3 +1,23 @@
-pub fn configure() {
-    todo!()
+use slog::{
+    Logger,
+    Drain,
+    o
+};
+
+/// Инициализатор логгера с компактным отображением
+pub fn configure_compact_root() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::CompactFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(drain, o!())
+}
+
+/// Инициализатор логгера с полным отображением
+pub fn configure_full_root() -> Logger {
+    let decorator = slog_term::TermDecorator::new().build();
+    let drain = slog_term::FullFormat::new(decorator).build().fuse();
+    let drain = slog_async::Async::new(drain).build().fuse();
+
+    slog::Logger::root(drain, o!())
 }
