77e90f697b5a3cf5680e061e8b7c940a8229c1ce
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-02 22:30:37 +0300

    Fix: several syntax mistakes

--- start.fish
+++ start.fish
@@ -1,7 +1,8 @@
-#/usr/bin/fish
+#!/usr/bin/fish
 
 set executable "queens_corsar"
+set logfile (date +%d-%m-%Y_%H-%M).txt
 
 echo "[start.sh] Bootstrapping queens_corsar bot"
-nohup ./queens_corsar >"$(date "+%d-%m-%Y_%H-%M").txt" & disown
+nohup ./queens_corsar >"$logfile" & disown
 echo "[start.sh] Bot launched in daemon mode"
