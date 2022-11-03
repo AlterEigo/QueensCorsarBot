00b948c6d0ea1517109e391eaa34ba0fa9e83faa
Iaroslav Sorokin <iaroslav.sorokin@gmail.com>
2022-11-03 12:52:01 +0300

    Mod: updated server side helper scripts

--- restart.fish
+++ restart.fish
@@ -1,11 +1,15 @@
 #!/usr/bin/fish
 
-set logfile (date +%d-%m-%Y_%H-%M).txt
-
 echo "[restart.fish] Killing possible running instance"
 pkill queens_corsar
 echo "[restart.fish] Waiting for 2 seconds just in case"
 sleep 2
+echo "[restart.fish] Moving log files into logs directory"
+for file in (ls *.txt)
+	if not lsof $file
+		mv $file -t logs/
+	end
+end
 echo "[restart.fish] Bootstrapping new instance"
-nohup ./queens_corsar >"$logfile" & disown
+./queens_corsar & disown
 echo "[restart.fish] Bot launched in daemon mode"
--- start.fish
+++ start.fish
@@ -1,8 +1,5 @@
 #!/usr/bin/fish
 
-set executable "queens_corsar"
-set logfile (date +%d-%m-%Y_%H-%M).txt
-
 echo "[start.sh] Bootstrapping queens_corsar bot"
-nohup ./queens_corsar >"$logfile" & disown
+./queens_corsar & disown
 echo "[start.sh] Bot launched in daemon mode"
